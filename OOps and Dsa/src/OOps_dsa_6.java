105d6f4cb26d2b34be112fd72e09bf1e82734dab
Popoye-75 <kasifmohd818@gmail.com>
2026-07-10 00:07:49 +0530

    Question 1 is solved by Popoye of oops dsa

---
+++ OOps and Dsa/src/OOps_dsa_6.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    // /* Question 1 ----> Sum of Array */
+    // public int sumArray(int[] arr) {
+    // int sum = 0;
+    // for (int i : arr) {
+    // sum += i;
+    // }
+    // return sum;
+    // }
+
+    // /* Question 2 -----> Reverse a String */
+
+}
+
+public class OOps_dsa_6 {
+    public static void main(String[] args) {
+        // /* Question 1 ----> Sum of Array */
+        // Solution sol = new Solution();
+        // int[] arr = { 1, 2, 3, 4, 5 };
+        // int result = sol.sumArray(arr);
+        // System.out.println("Sum ==> " + result);
+
+        // /* Question 2 -----> Reverse a String */
+        Solution sol = new Solution();
+
+    }
+}
