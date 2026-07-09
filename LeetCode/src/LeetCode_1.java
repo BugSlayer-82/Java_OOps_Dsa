c586445b3d4bc1695526850377a60152437587d4
Popoye-75 <kasifmohd818@gmail.com>
2026-07-09 20:16:49 +0530

    Updates from main repo

--- LeetCode/src/LeetCode_1.java
+++ LeetCode/src/LeetCode_1.java
@@ -1,2 +1,15 @@
-Add a new file called LeetCode_1
-now refresh setup in window
+// /* Question 1 ---> Two Sum Problem */
+class Solution {
+    public void printNum() {
+        System.out.println("This is num");
+    }
+}
+
+public class LeetCode_1 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println("This is main method");
+        sol.printNum();
+    }
+}
