670f581aacf879ccb40611b2129a115131e9d00f
BugSlayer-82 <mohdkashif9028@gmail.com>
2026-07-09 23:36:46 +0530

    Changes from window

--- LeetCode/src/LeetCode_1.java
+++ LeetCode/src/LeetCode_1.java
@@ -9,6 +9,7 @@ public class LeetCode_1 {
     public static void main(String[] args) {
         Solution sol = new Solution();
         System.out.println("In main class");
+        System.out.println("This is window");
         System.out.println("This is mac");
         sol.printNum();
     }
