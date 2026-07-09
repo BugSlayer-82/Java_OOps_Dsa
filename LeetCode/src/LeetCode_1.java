ebd4aaf444dc069eb56412e78addce00bc19d35f
BugSlayer-82 <mohdkashif9028@gmail.com>
2026-07-09 23:35:44 +0530

    changes from mac

--- LeetCode/src/LeetCode_1.java
+++ LeetCode/src/LeetCode_1.java
@@ -2,8 +2,6 @@
 class Solution {
     public void printNum() {
         System.out.println("This is num");
-        System.out.println("Second commit");
-        System.out.println("Corrected");
     }
 }
 
@@ -11,9 +9,7 @@ public class LeetCode_1 {
     public static void main(String[] args) {
         Solution sol = new Solution();
         System.out.println("In main class");
-        System.out.println("This is main method");
-        System.out.println("this is final");
-        System.out.println("This is from mac");
+        System.out.println("This is mac");
         sol.printNum();
     }
 }
