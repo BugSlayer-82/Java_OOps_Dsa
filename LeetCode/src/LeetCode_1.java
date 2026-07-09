e6ea8d814061f60a219c64878da87a4c73cc6d81
Popoye-75 <kasifmohd818@gmail.com>
2026-07-09 20:25:01 +0530

    Second changes from popoye

--- LeetCode/src/LeetCode_1.java
+++ LeetCode/src/LeetCode_1.java
@@ -8,7 +8,7 @@ class Solution {
 public class LeetCode_1 {
     public static void main(String[] args) {
         Solution sol = new Solution();
-
+        System.out.println("In main class");
         System.out.println("This is main method");
         sol.printNum();
     }
