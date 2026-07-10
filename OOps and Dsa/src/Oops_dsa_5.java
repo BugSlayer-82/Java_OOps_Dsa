74ec65232d899513f96fc95f60eb48d755e01a45
BugSlayer-82 <mohdkashif9028@gmail.com>
2026-07-11 01:08:43 +0530

    Changes from mac oops question is solved

--- OOps and Dsa/src/Oops_dsa_5.java
+++ OOps and Dsa/src/Oops_dsa_5.java
@@ -1,49 +1,112 @@
+// import java.util.Set;
+// import java.util.Map;
 // import java.util.HashMap;
+// import java.util.ArrayList;
 
-// /* Question ===> 1 Encapsulation, Constructor, fast lookup (Map hint) */
-// class Book {
-//     private String bookTitle;
-//     private String bookId;
-//     private boolean isAvailable;
-//     Book(String bookTitle, String bookId, boolean isAvailable) {
-//         this.bookTitle = bookTitle;
-//         this.bookId = bookId;
-//         this.isAvailable = isAvailable;
+// /* Question 2 =====> Employee Salary Register */
+// class Employee {
+//     private String employeeName;
+//     private String employeeId;
+//     private double salary;
+//     Employee(String employeeName, String employeeId, double salary) {
+//         this.employeeName = employeeName;
+//         this.employeeId = employeeId;
+//         this.salary = salary;
 //     }
-//     public String getBookTittle() {
-//         return bookTitle;
+//     public String getEmployeeName() {
+//         return employeeName;
 //     }
-//     public String getBookId() {
-//         return bookId;
+//     public String getEmployeeId() {
+//         return employeeId;
 //     }
-//     public boolean getStatus() {
-//         return isAvailable;
+//     public void setSalary(double salary) {
+//         this.salary = salary;
 //     }
-//     public String getBookDetail() {
-//         return "Book ID: " + bookId + " | Title: " + bookTitle + " | Available: " + isAvailable;
+//     public double getSalary() {
+//         return salary;
 //     }
 // }
+// class Company {
+//     private String companyName;
+//     private HashMap<String, Employee> register = new HashMap<>();
+//     private static Company company;
+//     private Company() {
+//     } // Singleton Pattern
+//     public static Company getCompany() { // Method to create only one object
+//         if (company == null) {
+//             company = new Company();
+//         }
+//         return company;
+//     }
+//     public void setCompanyName(String companyName) {
+//         this.companyName = companyName;
+//     }
+//     public String getCompanyName() {
+//         return companyName;
+//     }
+//     public void addEmployee(Employee e) {
+//         String id = e.getEmployeeId();
+//         register.put(id, e);
+//     }
+//     public double retrieveSalary(String employeeId) {
+//        Employee emp = register.get(employeeId);
+//         if (emp == null) {
+//             System.out.println("Employee doesn't exist ..!");
+//             return -1;
+//         }
+//         return emp.getSalary();
+//     }
+//     public String getEmployee(String id) {
+//         Employee emp = register.get(id);
+//         return "Employee Name : " + emp.getEmployeeName() + ", Employee Id : " + emp.getEmployeeId();
+//     }
+// }
+
+// /* Question 1 ===> Encapsulation, Constructor, fast lookup (Map hint) */
+// class Book {
+// private String bookTitle;
+// private String bookId;
+// private boolean isAvailable;
+// Book(String bookTitle, String bookId, boolean isAvailable) {
+// this.bookTitle = bookTitle;
+// this.bookId = bookId;
+// this.isAvailable = isAvailable;
+// }
+// public String getBookTittle() {
+// return bookTitle;
+// }
+// public String getBookId() {
+// return bookId;
+// }
+// public boolean getStatus() {
+// return isAvailable;
+// }
+// public String getBookDetail() {
+// return "Book ID: " + bookId + " | Title: " + bookTitle + " | Available: " +
+// isAvailable;
+// }
+// }
 // class Library {
-//     private String libraryName;
-//     private HashMap<String, Book> books = new HashMap<>();
-//     Library(String libraryName) {
-//         this.libraryName = libraryName;
-//     }
-//     public void addBook(Book b) {
-//         books.put(b.getBookId(), b);
-//     }
-//     public String getLibraryName() {
-//         return libraryName;
-//     }
-//     public Book getBookWithId(String id) {
-//         return books.get(id);
-//     }
+// private String libraryName;
+// private HashMap<String, Book> books = new HashMap<>();
+// Library(String libraryName) {
+// this.libraryName = libraryName;
+// }
+// public void addBook(Book b) {
+// books.put(b.getBookId(), b);
+// }
+// public String getLibraryName() {
+// return libraryName;
+// }
+// public Book getBookWithId(String id) {
+// return books.get(id);
+// }
 // }
 
 public class Oops_dsa_5 {
     public static void main(String[] args) {
 
-        // /* Question ===> 1 Encapsulation, Constructor, fast lookup (Map hint) */
+        // /* Question 1 ===> Encapsulation, Constructor, fast lookup (Map hint) */
         // Book b1 = new Book("Hulk the fighter", "H-234", true);
         // Book b2 = new Book("Stark the avenger", "S-348", true);
         // Book b3 = new Book("Strange the magician", "ST-234", true);
@@ -59,5 +122,15 @@ public class Oops_dsa_5 {
         // System.out.println("Book not available !");
         // }
 
+        // /* Question 2 =====> Employee Salary Register */
+        // Employee e1 = new Employee("Tony", "T-323", 50000.50d);
+        // Employee e2 = new Employee("Strange", "S-857", 8000.50d);
+        // Company comp = Company.getCompany();
+        // comp.setCompanyName("Stark Industries");
+        // System.out.println("Company Name --> " + comp.getCompanyName());
+        // comp.addEmployee(e1);
+        // comp.addEmployee(e2);
+        // System.out.println(comp.getEmployee("S-857"));
+        // System.out.println("Salary --> " + comp.retrieveSalary("S-857"));
     }
 }
