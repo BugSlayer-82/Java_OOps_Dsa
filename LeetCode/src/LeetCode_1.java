544ff7ff0b9081a2e1c9aeedb0b35bc9f6817f3b
Popoye-75 <kasifmohd818@gmail.com>
2026-07-11 20:57:55 +0530

    Question 1 is solved

--- LeetCode/src/LeetCode_1.java
+++ LeetCode/src/LeetCode_1.java
@@ -1,17 +1,31 @@
+import java.util.HashMap;
+
 // /* Question 1 ---> Two Sum Problem */
 class Solution {
-    public int [] twoSum(int nums[],int target) {
+    HashMap<Integer, Integer> map = new HashMap<>();
 
-    return new int[]{};
+    public int[] twoSum(int nums[], int target) {
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp)) {
+                return new int[] { map.get(temp), i };
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[] {};
     }
 }
 
 public class LeetCode_1 {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int [] nums = {2,7,11,15};
-        int target = 9;
-        int [] result = sol.twoSum(nums,target);
-        System.out.println(result);
+        int[] nums = { 2, 7, 11, 15, 10 };
+        int target = 17;
+        int[] result = sol.twoSum(nums, target);
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 }
