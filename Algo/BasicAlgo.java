17d283a1227c7644acbd752a79c8daff6c875b84
BugSlayer-82 <mohdkashif9028@gmail.com>
2026-07-15 11:56:17 +0530

    Add prefix sum

--- Algo/BasicAlgo.java
+++ Algo/BasicAlgo.java
@@ -1,12 +1,22 @@
 public class BasicAlgo {
     // /* =============> Categories of Array <================  */
-    // /* Basic implementation of Prefix Sum Algorithm */
-    static void prefixSum(){
-        
+    // /* Basic implementation of Prefix Sum Algorithm  [Time & Space O(1) O(n) ] */
+    static int [] buildPrefix(int []arr){
+        int [] prefix = new int[arr.length];
+        prefix[0] = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            prefix[i] = prefix[ i - 1 ] + arr[i];
+        }
+        return prefix;
+    }
+    static int rangeSum(int [] prefix, int l,int r){
+        if(l == 0){
+            return prefix[r];
+        }
+        return prefix[r]-prefix[l - 1];
     }
 
-
-    // /* Basic implementation of Kadane's Algorithm (Standard Approach & best approach) */
+    // /* Basic implementation of Kadane's Algorithm (Standard Approach & best approach) [Time O(n)]*/
     static int kadaneStandard(int [] arr){
         int currSum = arr[0];
         int maxSum = arr[0];
@@ -43,5 +53,10 @@ public class BasicAlgo {
         int [] arr2 = {2,-10,12,-5,6};
         int result2 = kadaneStandard(arr2);
         System.out.println("Max Sum standard ==> "+ result2);
+
+        // /* Prefix Sum Approach */
+        int []arr3 = {8,12,10,2,6,15,7};
+        int []prefix = buildPrefix(arr3);
+        System.out.println("Prefix sum ==> " + rangeSum(prefix, 0,2));
     }
 }
