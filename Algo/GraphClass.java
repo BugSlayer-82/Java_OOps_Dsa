d3dbcaf5d6c6c4f481550085ff8866012da80d96
BugSlayer-82 <mohdkashif9028@gmail.com>
2026-07-22 22:50:12 +0530

    changes from mac

--- Algo/GraphClass.java
+++ Algo/GraphClass.java
@@ -7,49 +7,122 @@ import java.util.PriorityQueue;
 
 public class GraphClass {
 
-    // /* ========>> Dijkstra's Algorithm to print the shortest path (Greedy Algorithm) <<========= */
-    
+    // /* ========>> Dijkstra's Algorithm to print the shortest path (Greedy
+    // Algorithm) Time Complexity O(E + ElogV) <<========= */
+    static class Edge {
+        int src;
+        int dest;
+        int weig;
 
-    // /* ========>> Cycle detection in Undirected graph Time Complexity O(V + E) <<======= */
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.weig = w;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
+        graph[1].add(new Edge(1, 3, 7));
+        graph[1].add(new Edge(1, 2, 1));
+        graph[2].add(new Edge(2, 4, 3));
+        graph[3].add(new Edge(3, 5, 1));
+        graph[4].add(new Edge(4, 3, 2));
+        graph[4].add(new Edge(4, 5, 5));
+    }
+
+    public static class Pair implements Comparable<Pair> {
+        int node;
+        int dist;
+
+        public Pair(int n, int d) {
+            this.node = n;
+            this.dist = d;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            return this.dist - p2.dist;
+        }
+    }
+
+    public static void dijkstra(ArrayList<Edge>[] graph, int src, int V) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        int[] dist = new int[V];
+        for (int i = 0; i < V; i++) {
+            if (i != src) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+        boolean[] visit = new boolean[V];
+        pq.add(new Pair(0, 0));
+
+        while (!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            if (!visit[curr.node]) {
+                visit[curr.node] = true;
+                for (int i = 0; i < graph[curr.node].size(); i++) {
+                    Edge e = graph[curr.node].get(i);
+                    int u = e.src;
+                    int v = e.dest;
+                    if (dist[u] + e.weig < dist[v]) {
+                        dist[v] = dist[u] + e.weig;
+                        pq.add(new Pair(v, dist[v]));
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < V; i++) {
+            System.out.print(dist[i] + " ");
+        }
+    }
+
+    // /* ========>> Cycle detection in Undirected graph Time Complexity O(V + E)
+    // <<======= */
     // static class Edge {
-    //     int src;
-    //     int dest;
-    //     public Edge(int s, int d) {
-    //         this.src = s;
-    //         this.dest = d;
-    //     }
+    // int src;
+    // int dest;
+    // public Edge(int s, int d) {
+    // this.src = s;
+    // this.dest = d;
     // }
-    // public static boolean isCycleUndirected(ArrayList<Edge>[]graph,boolean []visit,int curr,int par){
-    //     visit[curr] = true;
-    //     for(int i = 0; i < graph[curr].size(); i++){
-    //         Edge e = graph[curr].get(i);
-    //         if(visit[e.dest] && e.dest != par){
-    //             return true;
-    //         }
-    //         else if(!visit[e.dest]){
-    //             if(isCycleUndirected(graph, visit, e.dest, curr)){
-    //                 return true;
-    //             }
-    //         }
-    //     }
-    //     return false;
+    // }
+    // public static boolean isCycleUndirected(ArrayList<Edge>[]graph,boolean
+    // []visit,int curr,int par){
+    // visit[curr] = true;
+    // for(int i = 0; i < graph[curr].size(); i++){
+    // Edge e = graph[curr].get(i);
+    // if(visit[e.dest] && e.dest != par){
+    // return true;
+    // }
+    // else if(!visit[e.dest]){
+    // if(isCycleUndirected(graph, visit, e.dest, curr)){
+    // return true;
+    // }
+    // }
+    // }
+    // return false;
     // }
     // public static void createGraph(ArrayList<Edge>[] graph) {
-    //     for (int i = 0; i < graph.length; i++) {
-    //         graph[i] = new ArrayList<>();
-    //     }
-    //     graph[0].add(new Edge(0, 1));
-    //     graph[0].add(new Edge(0, 4));
-    //     graph[1].add(new Edge(1, 0));
-    //     graph[1].add(new Edge(1, 2));
-    //     graph[1].add(new Edge(1, 4));
-    //     graph[2].add(new Edge(2, 1));
-    //     graph[2].add(new Edge(2, 3));
-    //     graph[3].add(new Edge(3, 2));
-    //     graph[4].add(new Edge(4, 0));
-    //     graph[4].add(new Edge(4, 1));
-    //     graph[4].add(new Edge(4, 5));
-    //     graph[5].add(new Edge(5, 4));
+    // for (int i = 0; i < graph.length; i++) {
+    // graph[i] = new ArrayList<>();
+    // }
+    // graph[0].add(new Edge(0, 1));
+    // graph[0].add(new Edge(0, 4));
+    // graph[1].add(new Edge(1, 0));
+    // graph[1].add(new Edge(1, 2));
+    // graph[1].add(new Edge(1, 4));
+    // graph[2].add(new Edge(2, 1));
+    // graph[2].add(new Edge(2, 3));
+    // graph[3].add(new Edge(3, 2));
+    // graph[4].add(new Edge(4, 0));
+    // graph[4].add(new Edge(4, 1));
+    // graph[4].add(new Edge(4, 5));
+    // graph[5].add(new Edge(5, 4));
     // }
 
     // /* ========>> Topological Sorting Time Complexity O(V+E) <<========== */
@@ -470,5 +543,13 @@ public class GraphClass {
         // ArrayList<Edge> []graph = new ArrayList[V];
         // createGraph(graph);
         // System.out.println(isCycleUndirected(graph, new boolean[V], 0, -1));
+
+        // /* ========>> Dijkstra's Algorithm to print the shortest path (Greedy
+        // Algorithm) <<========= */
+        int V = 6;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+        dijkstra(graph, 0, V);
+        System.out.println();
     }
 }
