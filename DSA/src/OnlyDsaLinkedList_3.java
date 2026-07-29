04fcb2de8aef08339ede8c7758382147bbe1ad66
BugSlayer-82 <mohdkashif9028@gmail.com>
2026-07-29 07:19:49 +0530

    some changes

--- DSA/src/OnlyDsaLinkedList_3.java
+++ DSA/src/OnlyDsaLinkedList_3.java
@@ -22,30 +22,40 @@ public class OnlyDsaLinkedList_3 {
         head = newNode;
     }
 
-    public void addLast(int data){
+    public void addLast(int data) {
         Node newNode = new Node(data);
-        if(head == null){
+        if (head == null) {
             newNode.next = head;
             head = newNode;
             return;
         }
         Node currNode = head;
-        while(currNode.next != null){
+        while (currNode.next != null) {
             currNode = currNode.next;
         }
         currNode.next = newNode;
         newNode = currNode;
     }
 
-    public void addAtIndex(int i,int data){
+    public void addAtPosition(int i, int data) {
         Node newNode = new Node(data);
-        if(head == null){
+        if (head == null) {
             newNode.next = head;
             head = newNode;
             return;
         }
-       // Node currNode = head;
+        int count = 0;
+        Node currNode = head;
+        while (currNode != null) {
+            if (count == i) {
+                currNode.next = newNode;
+                return;
+            }
+            currNode = currNode.next;
+            count++;
+        }
     }
+
     public void printList() {
         Node currNode = head;
         if (head == null) {
@@ -61,12 +71,13 @@ public class OnlyDsaLinkedList_3 {
 
     public static void main(String[] args) {
         OnlyDsaLinkedList_3 list = new OnlyDsaLinkedList_3();
-        list.addFirst(1);
+        list.addFirst(4);
         list.addFirst(3);
-        list.addFirst(7);
-        list.addFirst(9);
-        list.addLast(4);
+        list.addFirst(2);
+        list.addFirst(1);
         list.addLast(6);
+        list.addLast(7);
+        list.addAtPosition(2, 5);
         list.printList();
     }
 }
