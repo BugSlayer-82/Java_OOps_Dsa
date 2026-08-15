6b638ba6e7bd6f8baca512f48011982a83ad424b
Popoye-75 <kasifmohd818@gmail.com>
2026-08-15 15:40:30 +0530

    updated code from linux

--- DSA/src/OnlyDsaStack_1.java
+++ DSA/src/OnlyDsaStack_1.java
@@ -5,41 +5,39 @@ import java.util.Stack;
 public class OnlyDsaStack_1 {
 
     // /* Question 8 ---> Evaluate Postfix */
-    static int evaluatePostfix(String exp){
-        Stack<Character> stack = new Stack<>();
-        Stack<Integer> number = new Stack<>();
-        System.out.println(stack);
-        System.out.println(number);
-        return 0;
+    static int evaluatePostfix(String exp) {
+        int sum = 0;
+    
+        return sum;
     }
 
     // /* Question 7 ---> Copy the Stack to another */
     // static void copyStack(Stack<Integer> s1, Stack<Integer> s2) {
-    //     if (s1.isEmpty()) {
-    //         return;
-    //     }
-    //     int topElement = s1.pop();
-    //     copyStack(s1, s2);
-    //     s1.push(topElement);
-    //     s2.push(topElement);
+    // if (s1.isEmpty()) {
+    // return;
+    // }
+    // int topElement = s1.pop();
+    // copyStack(s1, s2);
+    // s1.push(topElement);
+    // s2.push(topElement);
     // }
 
     // /* Question 6 ---> Print and empty Stack */
     // static void printStack(int []arr){
-    //     Stack <Integer> stack = new Stack<>();
-    //     for(int i : arr){
-    //         stack.push(i);
-    //     }
-    //     System.out.println(stack);
-    //     if(stack.isEmpty()){
-    //         System.out.println("null");
-    //     }
-    //     while(!stack.isEmpty()){
-    //         stack.pop();
-    //     }
-    //     System.out.println(stack);
+    // Stack <Integer> stack = new Stack<>();
+    // for(int i : arr){
+    // stack.push(i);
     // }
- 
+    // System.out.println(stack);
+    // if(stack.isEmpty()){
+    // System.out.println("null");
+    // }
+    // while(!stack.isEmpty()){
+    // stack.pop();
+    // }
+    // System.out.println(stack);
+    // }
+
     // /* Question 5 ---> Largest element in Stack */
     // static int largetElement(int[] arr) {
     // Stack<Integer> stack = new Stack<>();
@@ -161,7 +159,7 @@ public class OnlyDsaStack_1 {
         // Stack<Integer> s2 = new Stack<>();
         // int[] arr = { 2, 3, 5, 4, 8, 6, 7 };
         // for (int i : arr) {
-        //     s1.push(i);
+        // s1.push(i);
         // }
         // copyStack(s1, s2);
         // System.out.println(s1);
@@ -171,5 +169,6 @@ public class OnlyDsaStack_1 {
         String exp = "35+*24+3-";
         int result = evaluatePostfix(exp);
         System.out.println();
+        
     }
 }
