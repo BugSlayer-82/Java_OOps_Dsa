ab929af1cd9b3c9fcbca8bd2e3b7cf2ddbc079e0
Popoye-75 <kasifmohd818@gmail.com>
2026-08-23 17:19:08 +0530

    changes form linix

--- DSA/DSA_2/ArrayDs.java
+++ DSA/DSA_2/ArrayDs.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
+
 public class ArrayDs {
+
+    // /* Question 2 ==> To rotate the array to the right by k positions */
+    private static void reverse(int[] arr, int k) {
+        int start = 0, end = k - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void rotateArray(int[] arr, int k) {
+        // int start = 0, end = arr.length - 1;
+        reverse(arr,k);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 
     // /* Question 1 ==> Find the Second largest Distinct element from an array
     public static int secondLargest(int arr[]) {
@@ -16,9 +39,17 @@ public class ArrayDs {
     }
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         // /* Question 1 ==> Find the Second largest Distinct element from an array */
-        int arr[] = { 3, 6, 2, 9, 8, 7, 1, 6, 2 };
-        System.out.println("Second largest ==> " + secondLargest(arr));
+        // int []arr = { 3, 6, 2, 9, 8, 7, 1, 6, 2 };
+        // System.out.println("Second largest ==> " + secondLargest(arr));
+
+        // /* Question 2 ==> To rotate the array to the right by k positions */
+        System.out.print("Enter the position : ");
+        int k = sc.nextInt();
+        int[] arr = { 1, 2, 3, 4, 5 };
+        rotateArray(arr, k);
+
+        sc.close();
     }
 }
