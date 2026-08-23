6cce82e02ab2f6f497c8c0c93a8fc6d6d07ae690
Popoye-75 <kasifmohd818@gmail.com>
2026-08-23 17:47:07 +0530

    Updated code from linux

--- DSA/DSA_2/ArrayDs.java
+++ DSA/DSA_2/ArrayDs.java
@@ -1,44 +1,44 @@
 import java.util.Scanner;
 
 public class ArrayDs {
+    
 
     // /* Question 2 ==> To rotate the array to the right by k positions */
-    private static void reverse(int[] arr, int start,int end) {
-        while (start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
-    }
-
-    public static void rotateArray(int[] arr, int k) {
-        int n = arr.length;
-        k = k % n;
-        reverse(arr,0,n - 1);
-        reverse(arr,0,k - 1);
-        reverse(arr,k, n - 1);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
+    // private static void reverse(int[] arr, int start,int end) {
+    //     while (start < end) {
+    //         int temp = arr[start];
+    //         arr[start] = arr[end];
+    //         arr[end] = temp;
+    //         start++;
+    //         end--;
+    //     }
+    // }
+    // public static void rotateArray(int[] arr, int k) {
+    //     int n = arr.length;
+    //     k = k % n;
+    //     reverse(arr,0,n - 1);
+    //     reverse(arr,0,k - 1);
+    //     reverse(arr,k, n - 1);
+    //     for (int i = 0; i < arr.length; i++) {
+    //         System.out.print(arr[i] + " ");
+    //     }
+    //     System.out.println();
+    // }
 
     // /* Question 1 ==> Find the Second largest Distinct element from an array
-    public static int secondLargest(int arr[]) {
-        int max = Integer.MIN_VALUE;
-        int secMax = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                secMax = max;
-                max = arr[i];
-            } else if ((arr[i] != max) && (arr[i] > secMax)) {
-                secMax = arr[i];
-            }
-        }
-        return secMax;
-    }
+    // public static int secondLargest(int arr[]) {
+    //     int max = Integer.MIN_VALUE;
+    //     int secMax = Integer.MIN_VALUE;
+    //     for (int i = 0; i < arr.length; i++) {
+    //         if (arr[i] > max) {
+    //             secMax = max;
+    //             max = arr[i];
+    //         } else if ((arr[i] != max) && (arr[i] > secMax)) {
+    //             secMax = arr[i];
+    //         }
+    //     }
+    //     return secMax;
+    // }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -47,10 +47,10 @@ public class ArrayDs {
         // System.out.println("Second largest ==> " + secondLargest(arr));
 
         // /* Question 2 ==> To rotate the array to the right by k positions */
-        System.out.print("Enter the position : ");
-        int k = sc.nextInt();
-        int[] arr = { 1, 2, 3, 4, 5 };
-        rotateArray(arr, k);
+        // System.out.print("Enter the position : ");
+        // int k = sc.nextInt();
+        // int[] arr = { 1, 2, 3, 4, 5 };
+        // rotateArray(arr, k);
 
         sc.close();
     }
