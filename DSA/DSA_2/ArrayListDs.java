a4b04f2d302b70652186e8b2f81ad5497804e629
BugSlayer-82 <mohdkashif9028@gmail.com>
2026-08-28 18:01:02 +0530

    Update from window

--- DSA/DSA_2/ArrayListDs.java
+++ DSA/DSA_2/ArrayListDs.java
@@ -1,12 +1,28 @@
 import java.util.ArrayList;
 
-public class ArrayListDs{
-    // /* Question 1 ==> Remove all the duplicate */
-    public static void removeDuplicates(ArrayList<Integer> list){
-       
+
+public class ArrayListDs {
+   
+    public static void removeDuplicates(ArrayList<Integer> list) {
+        ArrayList<Integer> newList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            int current = list.get(i);
+            boolean isDuplicate = false;
+            for (int j = 0; j < newList.size(); j++) {
+                if(newList.get(j).equals(current)){
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if(!isDuplicate){
+                newList.add(current);
+            }
+        }
+
+        System.out.println(newList);
     }
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
         // /* Question 1 ==> Remove all the duplicate */
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
---
+++ DSA/DSA_2/StackDs.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class StackDs {
+     // /* Question 1 ==> Valid parenthesis */
+     static boolean isValid(String str){
+        Stack<Character> stack = new Stack<>();
+        
+        return false;
+     }
+    public static void main(String[]args){
+
+
+    }
+}
