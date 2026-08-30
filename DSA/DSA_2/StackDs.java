7c04bd77c5b4d0d8da834fdaa3807297d1fac06b
BugSlayer-82 <mohdkashif9028@gmail.com>
2026-08-30 23:43:08 +0530

    Changes from window

--- DSA/DSA_2/StackDs.java
+++ DSA/DSA_2/StackDs.java
@@ -1,14 +1,30 @@
 import java.util.Stack;
 
 public class StackDs {
-     // /* Question 1 ==> Valid parenthesis */
-     static boolean isValid(String str){
-        Stack<Character> stack = new Stack<>();
-        
-        return false;
-     }
-    public static void main(String[]args){
+   // /* Question 1 ==> Valid parenthesis */
+   static boolean isValid(String str) {
+      Stack<Character> stack = new Stack<>();
+      for (int i = 0; i < str.length(); i++) {
+         char ch = str.charAt(i);
+         if ((ch == '(') || (ch == '{') || (ch == '[')) {
+            stack.push(ch);
+         } else {
+            if(stack.isEmpty()){
+               return false;
+            }
+            char top = stack.pop();
+            if ((top == '(' && ch != ')') || (top == '{' && ch != '}') || (top == '[' && ch != ']')) {
+               return false;
+            }
+         }
+      }
+      return stack.isEmpty();
+   }
 
+   public static void main(String[] args) {
+      // /* Question 1 ==> Valid parenthesis */
+      String str = "({[]})";
+      System.out.println("Valid : " + isValid(str));
 
-    }
+   }
 }
